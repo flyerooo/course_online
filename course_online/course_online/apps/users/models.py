20a464acb7958f5b93d376d0e6dd1f07d428bb00
wangxiaolong <wangxiaolong@zhuotaiju.com>
2019-05-14 00:20:05 +0800

    增加apps 和 model

---
+++ course_online/course_online/apps/users/models.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class UserProfile(AbstractUser):
+
+    SEX_ITEMS = [
+        (1, '男'),
+        (2, '女'),
+        (0, '未知'),
+    ]
+    nick_name = models.CharField(max_length=50, verbose_name="昵称", default="")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.IntegerField(choices=SEX_ITEMS, verbose_name='性别')
+    address = models.CharField(max_length=100, default="")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to="image/%Y/%m", default="image/default.png", max_length=100)
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+        # def unread_nums(self):
+        #     # 获取用户未读消息的数量
+        #     from operation.models import UserMessage
+        #     return UserMessage.objects.filter(user=self.id, has_read=False).count()
+
+
+class EmailVerifyRecord(models.Model):
+    TYPE_REGISTER = 1
+    TYPE_FORGET = 0
+    TYPE_UPDATE_EMAIL = 2
+    SEND_TYPE = [
+        (TYPE_REGISTER, u"注册"),
+        (TYPE_FORGET, u"找回密码"),
+        (TYPE_UPDATE_EMAIL, u"修改邮箱"),
+    ]
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.IntegerField(verbose_name="验证码类型", choices=SEND_TYPE)
+    send_time = models.DateTimeField(verbose_name="发送时间", default=datetime.now)
+
+    class Meta:
+        verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name=u"轮播图", max_length=100)
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
--- course_online/course_online/settings.py
+++ course_online/course_online/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'course_online/apps'))  # 把apps加到搜索路径下，使得命令行运行时不报错ModuleNotFoundError
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'users',
+    'courses',
+    'organization',
+    'operation',
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
